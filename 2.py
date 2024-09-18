fae36d4bde0938057769b0d8366f5c3ffacce124
MaksymZhabchyk <zabcikmaksim@gmail.com>
2024-09-18 21:47:58 +0300

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,24 +1,31 @@
-val = [1, "hello", 3.14, True, [1, 2, 3], 10, 20, 30]
-
-for item in val
-data_type = [type(item)]
-type_count = {}
-
-for t in data_type:
-    if t in type_count:
-        type_count[t] += 1
-    else:
-        type_count[t] = 1
-
-m_c_type = None
-m_c_count = 0
-
-for t, count in type_count.items():
-    if count > m_c_count:
-        m_c_type = t
-        m_c_count = count
-
-
-
-print("Тип даних, який зустрічається найчастіше:", m_c_type)
-print("Кількість появ:", m_c_count) 
+a = 'Hello world!'
+name ='Maxym'
+surname = 'Zabchyk'
+age = 17
+#print('Value а:', a, 'Type:', type(a))
+#print('Value name:', name, 'Type:', type(name))
+#print('Value surname:', surname, 'Type:', type(surname))
+#print('Value age:', age,'Type:', type(age))
+lst = [a, name, surname, age] # Список змінних
+lst1 = [] #Значення змінних  
+lst2 = [] #Типи змінні
+string = [] #змінні з str
+floatt = [] # float
+intt = [] # int
+for item in lst:
+     lst1.append(item) #додаю значення змінних в lst1
+     lst2.append(type(item)) #додаю типи змінних із lst в lst2
+     if item == str:
+          string.append(item)# якщо змінна типу стр додаю в список lst3
+     elif item == int:
+          intt.append(item)
+     elif item == float:
+          floatt.append(item)
+if len(string) > len(floatt) and len(string) > len(intt):
+     print('Більше значень із типом str')
+elif len(floatt) > len(string) and len(floatt) > len(intt):
+     print('Більше значень із типом float')
+else:
+     print('Більше значень із типом int')
+print (lst2)
+print (lst1)
